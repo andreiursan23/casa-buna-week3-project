4447ef83562a9921740bb6e007f331b7bf9e3452
andreiursan23 <ursanandrei@ymail.com>
2021-11-04 16:14:49 +0200

    Color scheme and fine adjustments

--- app/js/script.js
+++ app/js/script.js
@@ -1,25 +1,26 @@
-const btnHamburger = document.querySelector('#btnHamburger');
-const header = document.querySelector('.header');
-const body = document.querySelector('body');
-const fadeElems = document.querySelectorAll('.has-fade');
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const body = document.querySelector("body");
+const fadeElems = document.querySelectorAll(".has-fade");
 
-btnHamburger.addEventListener('click', function(){
-    console.log('click hamburger');
+btnHamburger.addEventListener("click", function () {
+  console.log("click hamburger");
 
-    if(header.classList.contains('open')){ //Close Hamburger menu
-        body.classList.remove('noscroll')
-        header.classList.remove('open');
-        fadeElems.forEach(function(element){
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');
-        });
-    }
-    else { //Open hamburger menu
-        body.classList.add('noscroll')
-        header.classList.add('open')
-        fadeElems.forEach(function(element){
-            element.classList.add('fade-in');
-            element.classList.remove('fade-out');
-        });
-    }
+  if (header.classList.contains("open")) {
+    //Close Hamburger menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    //Open hamburger menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.add("fade-in");
+      element.classList.remove("fade-out");
+    });
+  }
 });
